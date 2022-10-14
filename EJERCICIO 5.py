9cf2c7f95e35c8375c2fb2adb15f9ea642856e66
Claverias123 <al_amartinezc@iessierradelasvillas.org>
2022-10-14 10:26:54 +0200

    MODIFICACIÓN 5

--- EJERCICIO 5.py
+++ EJERCICIO 5.py
@@ -1,4 +1,7 @@
 #Escribe un programa que pida un nombre de usuario y una contraseña y si se ha introducido “pepe” y “asdasd” se indica “Has entrado al sistema”, sino se da un error.
+usu="pepe"
+con="asdasd"
+
 usu=input("Dime el usuario\n")
 con=input("Dime la contraseña\n")
 
